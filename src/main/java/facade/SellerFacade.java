fde9e65668dc6121c59fca67a00049371f54d52d
johannesMeier <maildejohannes.34@gmail.com>
2016-03-16 14:30:35 +0100

    add création produit (Facade)

--- src/main/java/facade/SellerFacade.java
+++ src/main/java/facade/SellerFacade.java
@@ -1,5 +1,19 @@
 package facade;
+import manager.ProductManager;
+
 
 public class SellerFacade {
 
+	// manager des Produits
+	private manProd ProductManager;
+	
+	//initialisation du manager
+	public SellerFacade() {
+		this.manProd = new ProductManager();
+	}
+	
+	public boolean createProduct(String name, int quantity, double price, String id) {
+		return this.manProd.createProduct(name, quantity, price, id);
+	}
+	
 }
