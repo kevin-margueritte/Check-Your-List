579ff05b008e85d59f32bbd65ae4a5f68eb5c284
Gaet69 <gaetan.philippe1@gmail.com>
2016-03-16 15:57:37 +0100

    model category
    class category (model)

---
+++ src/main/java/model/category/Category.java
@@ -0,0 +1,47 @@
+package model.category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Category {
+	private String name;
+	private String shortDescription;
+	private String detailedDescription;
+	
+	private List subcategories = new ArrayList();
+	
+	public Category(String name) {
+		this.name= name;
+	}
+	
+	public Category(String name, String shortDescription, String detailedDescription) {
+		this.name= name;
+		this.shortDescription = shortDescription;
+		this.detailedDescription = detailedDescription;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getShortDescription() {
+		return this.shortDescription;
+	}
+	
+	public String getDetailedDescription() {
+		return this.detailedDescription;
+	}
+	
+	public void setName(String name) {
+	    this.name = name;
+	}
+	
+	public void setShortDescription(String shortDescription) {
+	    this.shortDescription = shortDescription;
+	}
+	
+	public void setDetailedDescription(String detailedDescription) {
+	    this.detailedDescription = detailedDescription;
+	}
+	
+}
