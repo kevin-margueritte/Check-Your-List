3b776f4a877cd33be62734ad0885b7cb25e518b5
kevin.margueritte <kevin.mrgueritte@gmail.com>
2016-03-22 10:46:00 +0100

    add MenuUI

--- src/main/java/UI/MenuUI.java
+++ src/main/java/UI/MenuUI.java
@@ -1,20 +1,16 @@
 package UI;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.Component;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.border.EmptyBorder;
-import javax.swing.SpringLayout;
-import javax.swing.SwingConstants;
 
 public class MenuUI extends JFrame {
+	
+	private JTabbedPane tabbedPane;
 
-	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -36,26 +32,18 @@ public class MenuUI extends JFrame {
 	 * Create the frame.
 	 */
 	public MenuUI() {
-		JFrame f = new JFrame("Test JTabbedPane");
-	    f.setSize(320, 150);
-	    JPanel pannel = new JPanel();
-
-	    JTabbedPane onglets = new JTabbedPane(SwingConstants.TOP);
-
-	    JPanel onglet1 = new JPanel();
-	    JLabel titreOnglet1 = new JLabel("Onglet 1");
-	    onglet1.add(titreOnglet1);
-	    onglet1.setPreferredSize(new Dimension(300, 80));
-	    onglets.addTab("onglet1", onglet1);
-
-	    JPanel onglet2 = new JPanel();
-	    JLabel titreOnglet2 = new JLabel("Onglet 2");
-	    onglet2.add(titreOnglet2);
-	    onglets.addTab("onglet2", onglet2);
-
-	    //onglets.setOpaque(true);
-	    pannel.add(onglets);
-	    f.getContentPane().add(pannel);
-	    f.setVisible(true);
+		getContentPane().setLayout(null);
+		this.tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		getContentPane().add(tabbedPane);
+		this.initProposeCategory();
+		//getContentPane().add(tabbedPane);
+	}
+	
+	public void initProposeCategory() {
+		ProposeCategoryUI f = new ProposeCategoryUI();
+		Component comp = f.getContentPane();
+		this.setSize(f.getWidth() + 15, f.getHeight() + 30);
+		tabbedPane.setSize(f.getWidth(), f.getHeight());
+		tabbedPane.addTab("first", comp);
 	}
 }
--- src/main/java/UI/ProposeSubcategoryUI.java
+++ src/main/java/UI/ProposeSubcategoryUI.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.*;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
