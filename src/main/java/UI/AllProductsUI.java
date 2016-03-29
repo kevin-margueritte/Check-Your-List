c0c35734e2176acd5a72ac400178564c0bd7c957
johannesMeier <maildejohannes.34@gmail.com>
2016-03-29 18:31:35 +0200

    ntm

--- src/main/java/UI/AllProductsUI.java
+++ src/main/java/UI/AllProductsUI.java
@@ -10,6 +10,7 @@ import model.person.Seller;
 import model.person.JDBC.SellerJDBC;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -27,6 +28,7 @@ import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
 
 public class AllProductsUI extends JFrame implements ActionListener, MouseListener {
 	
@@ -80,6 +82,8 @@ public class AllProductsUI extends JFrame implements ActionListener, MouseListen
 
 		
 		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(380, 20));
+		
 		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("New label");
@@ -87,16 +91,28 @@ public class AllProductsUI extends JFrame implements ActionListener, MouseListen
 		panel.add(lblNewLabel);
 		
 		JLabel label = new JLabel("New label");
-		label.setBounds(106, 9, 77, 14);
+		label.setBounds(106, 9, 58, 14);
 		panel.add(label);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(351, 5, 41, 23);
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.setBounds(390, 5, 58, 23);
 		panel.add(btnNewButton);
 		
 		
 		JScrollPane scrollPane_1 = new JScrollPane(panel);
-		scrollPane_1.setBounds(22, 135, 402, 39);
+		
+		JLabel label_1 = new JLabel("New label");
+		label_1.setBounds(182, 9, 77, 14);
+		panel.add(label_1);
+		
+		JLabel label_2 = new JLabel("New label");
+		label_2.setBounds(266, 9, 46, 14);
+		panel.add(label_2);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(334, 6, 46, 20);
+		panel.add(spinner);
+		scrollPane_1.setBounds(22, 135, 484, 187);
 		getContentPane().add(scrollPane_1);
 		
 		JLabel lblNewLabel_1 = new JLabel("Category");
@@ -108,16 +124,24 @@ public class AllProductsUI extends JFrame implements ActionListener, MouseListen
 		getContentPane().add(lblSubcategory);
 		
 		JLabel lblName = new JLabel("Name");
-		lblName.setBounds(39, 103, 46, 14);
+		lblName.setBounds(39, 110, 46, 14);
 		getContentPane().add(lblName);
 		
 		JLabel lblPrix = new JLabel("Price");
-		lblPrix.setBounds(130, 102, 46, 14);
+		lblPrix.setBounds(132, 110, 46, 14);
 		getContentPane().add(lblPrix);
 		
 		JLabel lblSeller = new JLabel("Seller");
-		lblSeller.setBounds(210, 102, 46, 14);
+		lblSeller.setBounds(210, 110, 46, 14);
 		getContentPane().add(lblSeller);
+		
+		JLabel lblQuantity = new JLabel("Quantity");
+		lblQuantity.setBounds(360, 110, 46, 14);
+		getContentPane().add(lblQuantity);
+		
+		JLabel lblStock = new JLabel("Stock");
+		lblStock.setBounds(288, 110, 53, 14);
+		getContentPane().add(lblStock);
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
